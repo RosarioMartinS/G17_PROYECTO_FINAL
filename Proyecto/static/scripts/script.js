68d3daf9f8944721e6ea737ddffd01cfc9021b0a
GabiIacomini <gabaiaco2000@gmail.com>
2022-10-14 00:10:38 -0300

    Cambios

--- Proyecto/static/scripts/script.js
+++ Proyecto/static/scripts/script.js
@@ -1,5 +1,19 @@
 function agregarDiv() {
     let clase = document.getElementById("info-imagen");
-    clase.classList.add("info-imagen")
+    clase.classList.add("info-imagenes")
     console.log(clase)
+}
+
+function bordeInferior() {
+    let border1 = document.getElementById("estilo1").innerHTML;
+    let border2 = document.getElementById("estilo2").innerHTML;
+    let border3 = document.getElementById("estilo3").innerHTML;
+    
+    let borde1= String(border1);
+    let borde2= String(border2);
+    let borde3= String(border3);
+
+    if (borde1 == "Buscar"){
+        
+    }
 }
