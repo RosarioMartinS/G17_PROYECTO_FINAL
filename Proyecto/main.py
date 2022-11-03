9e4ab1914164286d9b2690fad2a79a0e2b5b706a
AugustoFlorio <101183545+AugustoFlorio@users.noreply.github.com>
2022-11-03 14:46:22 -0300

    cambios

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -81,6 +81,15 @@ def agregarUsuario():
 @app.route('/home', methods=['POST','GET'])
 def home():
     if request.method == "GET":
+        session["imagen"]         = ""
+        session["nombreRemera"]   = ""
+        session["precioRemera"]   = ""
+        session["nombreAbrigo"]   = ""
+        session["precioAbrigo"]   = ""
+        session["nombrePantalon"] = ""
+        session["precioPanalon"]  = ""
+        session["nombreSneaker"]  = ""
+        session["precioSneaker"]  = ""
         return render_template("base.html")
     elif request.method == "POST":
         return redirect('/home')
@@ -92,6 +101,19 @@ def profile():
     elif request.method == "POST":
         return redirect('/home')
 
+@app.route('/subirImagen', methods = ['POST', 'GET'])
+def nuevaImagen():
+    if request.method == "POST":
+        session["imagen"]         = request.form["imagen"]        
+        session["nombreRemera"]   = request.form["nombbreRemera"]
+        session["precioRemera"]   = request.form["precioRemera"]
+        session["nombreAbrigo"]   = request.form["nombreAbrigo"]
+        session["precioAbrigo"]   = request.form["precioAbrigo"]
+        session["nombrePantalon"] = request.form["nombrePantalon"]
+        session["precioPanalon"]  = request.form["precioPantalon"]
+        session["nombreSneaker"]  = request.form["nombreSneaker"]
+        session["precioSneaker"]  = request.form["precioSneaker"]
 
+        
 
 app.run(host='0.0.0.0', port=81)
